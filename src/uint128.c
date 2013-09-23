b0e7f0d15352cd7b3a294dde340519b1f42e24ac
mzhel <bitcatcher@fastmail.fm>
2013-09-23 14:27:24 +0400

    uint128 module implemented.

--- src/uint128.c
+++ src/uint128.c
@@ -85,3 +85,536 @@ uint128_xor(UINT128* first, UINT128* second, UINT128* xorRes)
 
   return result;
 }
+
+uint8_t
+uint128_set_bit_value(
+                      UINT128* ui128,
+                      uint32_t bit_idx,
+                      uint8_t bit_val
+                      )
+{
+  uint8_t result = 0;
+
+  do {
+
+    if (!ui128 || bit_idx > 127) break;
+
+    if (bit_val){
+
+      ui128->data.dwordData[3 - (bit_idx / 32)] |= 1 << (bit_idx % 32);
+
+    } else {
+
+      ui128->data.dwordData[3 - (bit_idx / 32)] &= ~(1 << (bit_idx % 32));
+
+    }
+
+    result = 1;
+
+  } while (false);
+
+  return result;
+}
+
+uint8_t
+uint128_get_bit_string(
+                       UINT128* ui128,
+                       char* bit_str_buf,
+                       uint32_t bit_str_buf_len
+                      )
+{
+  uint8_t result = 0;
+  uint32_t needed_len = 0;
+  uint32_t dw;
+  uint32_t k = 0;
+  
+  do {
+      
+    if (!ui128 || !bit_str_buf) break;
+
+    needed_len = UINT128_BYTES_COUNT * 8 + 1;
+
+    if (bit_str_buf_len < needed_len) break;
+
+    for (uint8_t i = 0; i < UINT128_DWORDS_COUNT; i++) {
+
+      dw = ui128->data.dwordData[i];
+
+      for (int8_t j = 31; j >= 0; j--) {
+
+        bit_str_buf[k++] = ((dw & (1 << j))?1:0) + 0x30;
+
+      }
+
+    }
+    
+    result = 1;
+
+  } while (false);
+
+  return result;
+
+}
+
+uint8_t
+uint128_get_bit_value(
+                      UINT128* ui128,
+                      uint32_t bit_idx
+                      )
+{
+  uint8_t result = 0;
+
+  do {
+
+    result = bit_idx < 128 ? (ui128->data.dwordData[3 - (bit_idx/32)] >> (bit_idx % 32)) & 1 : 0;
+
+  } while (false);
+
+  return result;
+}
+
+uint8_t
+uint128_get_bit_value_reverse(
+                              UINT128* ui128,
+                              uint32_t bit_idx
+                             )
+{
+  uint8_t result = 0;
+
+  do {
+
+    if (!ui128) break;
+
+    result = bit_idx < 128 ? (ui128->data.dwordData[bit_idx/ 32] >> (31 - (bit_idx % 32))) & 1 : 0;
+
+
+  } while (false);
+
+  return result;
+}
+
+uint8_t
+uint128_set_bit_value_reverse(
+                              UINT128* ui128,
+                              uint32_t bit_idx,
+                              uint8_t bit_val
+                             )
+{
+  uint8_t result = 0;
+
+  do {
+
+    if (!ui128 || bit_idx > 127) break;
+
+    if (bit_val) {
+
+      ui128->data.dwordData[bit_idx / 32] |= 1 << (31 - (bit_idx % 32));
+
+    } else {
+      
+      ui128->data.dwordData[bit_idx / 32] &= ~(1 << (31 - (bit_idx % 32)));
+
+    }
+
+    result = 1;
+
+  } while (false);
+
+  return result;
+}
+
+uint8_t
+uint128_compare(
+                UINT128* f,
+                UINT128* s
+               )
+{
+  uint8_t result = 0;
+
+  do {
+    
+    if (!f || !s) break;
+
+    for (uint8_t i = 0; i < 4; i++){
+
+      if (f->data.dwordData[i] > s->data.dwordData[i]){
+
+        result = 1;
+
+        break;
+
+      } else if (f->data.dwordData[i] < s->data.dwordData[i]) {
+        
+        result = 0xff;
+
+        break;
+
+      }
+
+    }
+
+  } while (false);
+
+  return result;
+}
+
+uint8_t
+uint128_shift_left(
+                   UINT128* ui128,
+                   uint8_t bit_count,
+                   UINT128* ui128_res
+                  )
+{
+  uint8_t result = 0;
+  int32_t idx_shift = 0;
+  int64_t shifted = 0;
+  int32_t i = 0;
+  uint32_t shift_res[4] = {0};
+
+  do {
+
+    if (!ui128) break;
+
+    if (bit_count > 127) {
+
+      memset(ui128, 0, sizeof(UINT128));
+
+    } else {
+
+      idx_shift = bit_count / 32;
+
+      for (i = 3; i >= idx_shift; i--){
+
+        shifted += ((int64_t)ui128->data.dwordData[i]) << (bit_count % 32);
+
+        shift_res[i - idx_shift] = (uint32_t)shifted;
+
+        shifted >>= 32;
+
+      }
+
+      if (ui128_res) {
+
+        memcpy(ui128_res, shift_res, sizeof(UINT128));
+
+      } else {
+        
+        memcpy(ui128, shift_res, sizeof(UINT128));
+
+      }
+
+    }
+
+  } while (false);
+
+  return result;
+}
+
+uint8_t
+uint128_add(
+            UINT128* f,
+            UINT128* s,
+            UINT128* r
+            )
+{
+  uint8_t result = 0;
+  LARGE_INT sum = {0};
+  uint32_t sum_res[4] = {0};
+
+  do {
+
+    if (!f || !s) break;
+
+    for (int8_t i = 3; i >= 0; i--) {
+      
+      sum.quad_part += f->data.dwordData[i];
+
+      sum.quad_part += s->data.dwordData[i];
+
+      sum_res[i] = sum.low_part;
+
+      sum.low_part = sum.high_part;
+
+      sum.high_part = 0;
+
+    }
+
+    if (r) {
+
+      memcpy(r, sum_res, sizeof(UINT128));
+
+    } else {
+
+      memcpy(f, sum_res, sizeof(UINT128));
+      
+    }
+
+    result = 1;
+
+  } while (false);
+
+  return result;
+}
+
+uint8_t
+uint128_add_dword(
+                  UINT128* f,
+                  uint32_t dw,
+                  UINT128* r
+                 )
+{
+  uint8_t result = 0;
+  UINT128 s;
+
+  do {
+
+    memset(&s, 0, sizeof(s));
+
+    s.data.dwordData[3] = dw;
+
+    result = uint128_add(f, &s, r);
+
+  } while (false);
+
+  return result;
+}
+
+uint8_t
+uint128_substract(
+                  UINT128* f,
+                  UINT128* s,
+                  UINT128* r
+                 )
+{
+  uint8_t result = 0;
+  LARGE_INT sub = {0};
+  uint32_t sub_res[4] = {0};
+  uint8_t carry = 0;
+
+  do {
+
+     if (!f || !s) break;
+
+     for (int8_t i = 3; i >= 0; i--) {
+      
+       sub.quad_part += f->data.dwordData[i] - carry;
+
+       sub.quad_part -= s->data.dwordData[i];
+
+       sub_res[i] = sub.low_part;
+
+       if ((int32_t)(f->data.dwordData[i] - carry) < (int32_t)s->data.dwordData[i]) carry = 1; else carry = 0;
+
+       sub.high_part = sub.low_part = 0;
+
+     }
+      
+     if (r) {
+
+       memcpy(r, sub_res, sizeof(UINT128));
+
+     } else {
+      
+       memcpy(f, sub_res, sizeof(UINT128));
+
+     }
+
+    result = 1;
+
+  } while (false);
+
+  return result;
+}
+
+uint8_t
+uint128_substract_dword(
+                        UINT128* f,
+                        uint32_t dw,
+                        UINT128* r
+                       )
+{
+  uint8_t result = 0;
+  UINT128 s;
+
+  do {
+
+    memset(&s, 0, sizeof(s));
+
+    s.data.dwordData[3] = dw;
+
+    result = uint128_substract(f, &s, r);
+
+  } while (false);
+
+  return result;
+}
+
+uint8_t
+uint128_compare_dword(
+                      UINT128* f,
+                      uint32_t dw
+                     )
+{
+  uint8_t result = 0;
+
+  do {
+
+   if (f->data.dwordData[0] > 0 || f->data.dwordData[1] > 0 || f->data.dwordData[2] > 0 || f->data.dwordData[3] > dw) result = 1;
+
+   else if (f->data.dwordData[3] < dw) result = 0xff;
+
+   else result = 0;
+
+  } while (false);
+
+  return result;
+}
+
+uint8_t
+uint128_init(
+             UINT128* f,
+             uint8_t v
+            )
+{
+  uint8_t result = 0;
+
+  do {
+
+    if (!f) break;
+
+    memset(f, v, sizeof(UINT128));
+
+    result = 1;
+
+  } while (false);
+
+  return result;
+}
+
+uint8_t
+uint128_copy_bits_be(
+                     UINT128* s,
+                     UINT128* d,
+                     uint32_t num_bits,
+                     bool rand_rem
+                     )
+{
+  uint8_t result = 0;
+  uint32_t num = 0;
+
+
+  do {
+
+    if (!s || !d) break;
+
+    num = num_bits / 32;
+
+    for (uint8_t i = 0; i < num; i++){
+
+      d->data.dwordData[i] = s->data.dwordData[i];
+
+    }
+
+    for (uint8_t i = (32 * num); i < num_bits; i++){
+
+      uint128_set_bit_value_reverse(
+                                    d,
+                                    i,
+                                    uint128_get_bit_value_reverse(s, i)
+                                    );
+
+    }
+
+    if (rand_rem){
+
+      for (uint8_t i = num_bits; i < 128; i++){
+
+        uint128_set_bit_value_reverse(d, i, random_uint32() % 2);
+
+      }
+
+    }
+
+    result = 1;
+  
+  } while (false);
+
+  return result;
+}
+
+uint8_t
+uint128_emit(
+             UINT128* ui128,
+             void* buf,
+             uint32_t buf_size
+            )
+{
+  uint8_t result = 0;
+
+  do {
+
+    if (!ui128 || !buf) break;
+
+    if (buf_size < sizeof(UINT128)) break;
+
+    memcpy(buf, ui128, sizeof(UINT128));
+
+    result = 1;
+
+  } while (false);
+
+  return result;
+}
+
+uint8_t
+uint128_emit_be(
+                UINT128* ui128,
+                void* buf,
+                uint32_t buf_size
+               )
+{
+  uint32_t result = 0;
+  uint32_t* p = NULL;
+
+  do {
+
+    if (!ui128 || !buf) break;
+
+    if (buf_size < sizeof(UINT128)) break;
+
+    p = (uint32_t*)buf;
+
+    for (uint8_t i = 0; i < UINT128_DWORDS_COUNT; i++){
+
+      p[i] = BSWAP32(ui128->data.dwordData[i]);
+
+    }
+
+    result = 1;
+
+  } while (false);
+
+  return result;
+}
+
+uint8_t
+uint128_copy(
+             UINT128* s,
+             UINT128* d
+            )
+{
+  uint8_t result = 0;
+
+  do {
+
+    if (!s || !d) break;
+
+    memcpy(d, s, sizeof(UINT128));
+
+    result = 1;
+
+  } while (false);
+
+  return result;
+}
+
--- tests/uint128_tests.c
+++ tests/uint128_tests.c
@@ -3,6 +3,7 @@
 #include <setjmp.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <memory.h>
 #include <uint128.h>
 #include <random.h>
 #include <cmockery.h>
@@ -33,7 +34,7 @@ test_uint128_from_buffer(void **state)
                       false
                       );
 
-  LOG_DEBUG_UINT128("uint128:", (UINT128*)(&ui128));
+  LOG_DEBUG_UINT128("uint128:", ((UINT128*)&ui128));
 
   uint128_from_buffer(
                       &ui128,
@@ -42,11 +43,11 @@ test_uint128_from_buffer(void **state)
                       true
                       );
 
-  LOG_DEBUG_UINT128("uint128:", (UINT128*)(&ui128));
+  LOG_DEBUG_UINT128("uint128:", ((UINT128*)&ui128));
 
 }
 
-void test_generate_uint128(void **state)
+void test_uint128_generate(void **state)
 {
   UINT128 ui128;
 
@@ -54,18 +55,277 @@ void test_generate_uint128(void **state)
 
   uint128_generate(&ui128);
 
-  LOG_DEBUG_UINT128("Generated uint128", (&ui128));
+  LOG_DEBUG_UINT128("Generated uint128", ((UINT128*)&ui128));
 
 }
 
-void test_xor_uint128(void **state)
+void test_uint128_xor(void **state)
+{
+  uint8_t buffer[UINT128_BYTES_COUNT];
+  UINT128 f;
+  UINT128 s;
+  UINT128 r;
+
+  for (uint8_t i = 0; i < sizeof(buffer); i++) {
+
+    buffer[i] = i;
+
+  }
+
+  uint128_from_buffer(&f, buffer, sizeof(buffer), false);
+
+  uint128_from_buffer(&s, buffer, sizeof(buffer), false);
+
+  uint128_xor(&f, &s, &r);
+
+  LOG_DEBUG_UINT128("xor result:", ((UINT128*)&r));
+
+  for (uint8_t i = 0; i < UINT128_DWORDS_COUNT; i++){
+
+    assert_int_equal(r.data.dwordData[i], 0);
+
+  }
+  
+}
+
+void
+test_uint128_set_bit_value(void** state)
+{
+  UINT128 f;
+  uint32_t acc = 0;
+
+  memset(&f, 0, sizeof(UINT128));
+
+  for (uint8_t i = 0; i < UINT128_BYTES_COUNT * 8; i++) {
+
+    uint128_set_bit_value(&f, i, 1);
+
+    LOG_DEBUG_UINT128("uint128", ((UINT128*)&f));
+
+    acc <<= 1;
+
+    acc |= 1;
+
+    assert_int_equal(f.data.dwordData[3 - i / 32], acc);
+
+    if (acc == 0xffffffff) acc = 0;
+
+  }
+  
+}
+
+void
+test_uint128_get_bit_string(void** state)
+{
+  UINT128 f;
+  char buf[UINT128_BYTES_COUNT * 8 + 1];
+
+
+  memset(&f, 0, sizeof(UINT128));
+
+  memset(buf, 0, sizeof(buf));
+
+  for (uint8_t i = 0; i < UINT128_BYTES_COUNT * 8; i++){
+
+    uint128_set_bit_value(&f, i, 1);
+
+    assert_int_equal(1, uint128_get_bit_string(&f, buf, sizeof(buf)));
+
+    assert_int_equal(0x31, buf[UINT128_BYTES_COUNT * 8 - 1 - i]);
+
+    LOG_DEBUG_UINT128("uint128", ((UINT128*)&f))
+
+    LOG_DEBUG("%s", buf);
+
+  }
+  
+}
+
+void
+test_uint128_get_bit_value(void** state)
+{
+  UINT128 f;
+
+  memset(&f, 0, sizeof(f));
+
+  for (uint8_t i = 0; i < (UINT128_BYTES_COUNT * 8); i++) {
+
+    uint128_set_bit_value(&f, i, 1);
+
+    LOG_DEBUG_UINT128("uint128", ((UINT128*)&f));
+      
+    assert_int_equal(uint128_get_bit_value(&f, i), 1);
+
+  }
+
+}
+
+void
+test_uint128_get_bit_value_reverse(void** state)
+{
+  UINT128 f;
+
+  memset(&f, 0, sizeof(f));
+
+  for (uint8_t i = 0; i < UINT128_BYTES_COUNT * 8; i++){
+  
+    uint128_set_bit_value(&f, i, 1);
+
+    LOG_DEBUG_UINT128("uint128", ((UINT128*)&f));
+
+    assert_int_equal(1, uint128_get_bit_value_reverse(&f, UINT128_BYTES_COUNT * 8 - 1 - i));
+
+  }
+
+}
+
+void
+test_uint128_set_bit_value_reverse(void** state)
+{
+  UINT128 f;
+
+  memset(&f, 0, sizeof(f));
+  
+  for (uint8_t i = 0; i < UINT128_BYTES_COUNT * 8; i++){
+  
+    uint128_set_bit_value_reverse(&f, i, 1);
+
+    LOG_DEBUG_UINT128("uint128", ((UINT128*)&f));
+
+    assert_int_equal(1, uint128_get_bit_value_reverse(&f, i));
+
+  }
+
+}
+
+void
+test_uint128_compare(void** state)
+{
+  UINT128 f;
+  UINT128 s;
+
+  memset(&f, 0, sizeof(f));
+
+  memset(&s, 0, sizeof(f));
+
+  uint128_set_bit_value_reverse(&f, 2, 1);
+
+  assert_int_equal(1, uint128_compare(&f, &s));
+
+  uint128_set_bit_value_reverse(&s, 1, 1);
+
+  assert_int_equal(0xff, uint128_compare(&f, &s));
+
+}
+
+void
+test_uint128_shift_left(void** state)
+{
+  UINT128 f;
+  UINT128 s;
+
+  memset(&f, 0, sizeof(f));
+
+  memset(&s, 0, sizeof(s));
+
+  uint128_set_bit_value(&f, 0, 1);
+
+  uint128_set_bit_value(&s, 5, 1);
+
+  LOG_DEBUG_UINT128("before shift", ((UINT128*)&f));
+
+  LOG_DEBUG_UINT128("before shift", ((UINT128*)&s));
+
+  uint128_shift_left(&f, 6, NULL);
+
+  uint128_shift_left(&s, 1, NULL);
+
+  LOG_DEBUG_UINT128("after shift", ((UINT128*)&f));
+
+  LOG_DEBUG_UINT128("after shift", ((UINT128*)&s));
+
+  assert_int_equal(0, uint128_compare(&f, &s));
+
+}
+
+void
+test_uint128_add(void** state)
 {
   UINT128 f;
   UINT128 s;
   UINT128 r;
 
+  memset(&f, 0, sizeof(f));
+
+  memset(&s, 0, sizeof(s));
+
+  f.data.dwordData[3] = 0xffffffff;
+
+  s.data.dwordData[3] = 0xffffffff;
+
+  uint128_add(&f, &s, &r);
+
+  assert_int_equal(0xfffffffe, r.data.dwordData[3]);
+
+  assert_int_equal(0x1, r.data.dwordData[2]);
+
+  LOG_DEBUG_UINT128("f:", ((UINT128*)&f));
+
+  LOG_DEBUG_UINT128("s:", ((UINT128*)&s));
+
+  LOG_DEBUG_UINT128("r:", ((UINT128*)&r));
+
+}
+
+void
+test_uint128_substract(void** state)
+{
+  UINT128 f;
+  UINT128 s;
+  UINT128 r;
   
-  
+  memset(&f, 0, sizeof(f));
+
+  memset(&s, 0, sizeof(s));
+
+  f.data.dwordData[3] = 1;
+
+  s.data.dwordData[3] = 2;
+
+  uint128_substract(&f, &s, &r);
+
+  LOG_DEBUG_UINT128("sub res:", ((UINT128*)&r)); 
+
+  for (uint8_t i = 0; i < UINT128_DWORDS_COUNT; i++){
+
+    assert_int_equal(0xffffffff, r.data.dwordData[i]);
+
+  }
+
+}
+
+void
+test_uint128_copy_bits_be(void** state)
+{
+  UINT128 s;
+  UINT128 d;
+  UINT128 d2;
+
+  uint128_init(&s, 0xff);
+
+  uint128_zero_init(&d);
+
+  uint128_copy_bits_be(&s, &d, 16, false);
+
+  assert_int_equal(0xffff, d.data.wordData[1]);
+
+  LOG_DEBUG_UINT128("dst", ((UINT128*)&d));
+
+  uint128_copy_bits_be(&s, &d2, 32, true);
+
+  assert_int_equal(0xffffffff, d2.data.dwordData[0]);
+
+  LOG_DEBUG_UINT128("dst2", ((UINT128*)&d2));
 }
 
 int main(int argc, char* argv[])
@@ -79,7 +339,19 @@ int main(int argc, char* argv[])
 
   const UnitTest tests[] = {
     unit_test(test_success),
-    unit_test(test_uint128_from_buffer)
+    unit_test(test_uint128_generate),
+    unit_test(test_uint128_from_buffer),
+    unit_test(test_uint128_xor),
+    unit_test(test_uint128_set_bit_value),
+    unit_test(test_uint128_get_bit_value),
+    unit_test(test_uint128_get_bit_value_reverse),
+    unit_test(test_uint128_set_bit_value_reverse),
+    unit_test(test_uint128_get_bit_string),
+    unit_test(test_uint128_compare),
+    unit_test(test_uint128_shift_left),
+    unit_test(test_uint128_add),
+    unit_test(test_uint128_substract),
+    unit_test(test_uint128_copy_bits_be)
   };
 
   return run_tests(tests);
